2ef10f4d3d21884c2344f115dda50b2ffeb797cb
Ingmar Steen <iksteen@gmail.com>
2015-04-12 14:21:47 +0200

    Clean up pelican config.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,23 +6,23 @@ AUTHOR = u'Ingmar Steen'
 SITENAME = u'Blurring Existence'
 SITESUBTITLE = u'Things are getting weirder at the speed of light...'
 SITEURL = ''
-THEME = 'themes/blurry'
-
-PATH = 'content'
-
 TIMEZONE = 'Europe/Amsterdam'
-
 DEFAULT_LANG = u'en'
+
+RELATIVE_URLS = True
+PATH = 'content'
+THEME = 'themes/blurry'
 
 PLUGINS = ['pelican_albums']
 ARTICLE_EXCLUDES = ['images']
-
 MD_EXTENSIONS = [
     'codehilite(css_class=highlight, guess_lang=False, linenums=False)',
     'extra',
 ]
 
+DEFAULT_PAGINATION = 10
 TYPOGRIFY = True
+THUMBNAIL_DEFAULT_SIZE = '192x192'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -67,9 +67,3 @@ LINKS = (
         ),
     ),
 )
-
-DEFAULT_PAGINATION = 10
-
-RELATIVE_URLS = True
-
-THUMBNAIL_DEFAULT_SIZE = '192x192'
